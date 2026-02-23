6ad3c4a5a69df462707d1a979441d77f312b2c14
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2026-02-23 12:45:45 +0700

    new commit

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    # Database SQLite
+    SQLALCHEMY_DATABASE_URI = "sqlite://ashuraarticle.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # For save image
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "uploads")
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # Max 16 MB
---
+++ backend/models/article.py
@@ -0,0 +1,18 @@
+from app import db
+from datetime import datetime
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False) # Judul artikel
+    content = db.Column(db.Text, nullable=False) # Isi artikel
+    thumbnail = db.Column(db.String(300), nullable=True) # Nama file gambar
+    created_at = db.Column(db.DateTime, default=datetime.utcnow) # Waktu dibuat
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "thumbnail": self.thumbnail,
+            "created_at": self.created_at.strftime("%d %B %Y")
+        }
